a5432bdec1f31acd0bd854a2317d2cfd32484ab8
Valter Silva <svalter222@gmail.com>
2022-04-25 23:52:47 -0300

    criação tela de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom'
 import RoutesApp from './routes';
-
+import './global.css'
 
 function App() {
   return (
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { FiMail, FiEye } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import styled from './SignIn.module.css'
+
+import Logo from '../../assets/logo/logo.png'
 
 function SignIn() {
-    return (
-      <div>
-        <p>Tela de SignIn</p>
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className={styled.container}>
+      <div className={styled.containerFormulario}>
+        <div className={styled.logo}>
+          <img src={Logo} alt="Logo Sistema de Chamados" className={styled.logoImg} />
+        </div>
+        <h2 className={styled.title}>Entrar</h2>
+        <form className={styled.formulario}>
+          <div className={styled.contentInputs}>
+            <input type="text" placeholder="Digite seu email" className={styled.inputs} />
+            <FiMail className={styled.icons} />
+          </div>
+          <div className={styled.contentInputs}>
+            <input type="password" placeholder="Digite sua senha" className={styled.inputs} />
+            <FiEye className={styled.icons} />
+          </div>
+          <button className={styled.buttonAccess}>Acessar</button>
+        </form>
+        <Link to="/register" className={styled.links}>Criar uma conta</Link>
+        <Link to="" className={styled.links}>Esqueci minha senha</Link>
       </div>
-    );
-  }
-  
-  export default SignIn;
-  
+    </div>
+  );
+}
+
+export default SignIn;
