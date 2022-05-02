f1822277dc42774db2ca049902ed367ff5b739e9
Valter Silva <svalter222@gmail.com>
2022-05-01 22:57:46 -0300

    mensagens de alertas

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import firebase from '../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({});
 
@@ -44,10 +45,12 @@ function AuthProvider({ children }) {
                 setUser(data);
                 storageUser(data);
                 setLoadingAuth(false);
+                toast.success('Bem vindo de volta!');
             })
             .catch((error) => {
                 console.log(error);
                 setLoadingAuth(false);
+                toast.error('Ops! Algo deu errado.');
             })
     }
 
@@ -74,11 +77,14 @@ function AuthProvider({ children }) {
                         setUser(data);
                         storageUser(data);
                         setLoadingAuth(false);
+                        toast.success('Bem vindo a plataforma!');
                     })
             })
             .catch((error) => {
                 console.log(error);
+                toast.error('Ops! Algo deu errado!');
                 setLoadingAuth(false);
+               
             })
     }
 
