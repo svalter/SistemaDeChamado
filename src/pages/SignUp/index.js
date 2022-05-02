fcf52699f4ee740f08379e53a3a5896ed3d1b42e
Valter Silva <svalter222@gmail.com>
2022-05-01 22:06:16 -0300

    rotas privadas

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/auth";
 import { FiMail, FiEye, FiEyeOff, FiUser } from 'react-icons/fi';
 import styled from './SignUp.module.css'
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/auth';
 import { useContext } from 'react';
-
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 
 
-
 export default function RoutesApp() {
-
-
-    function PrivateRoute({ children }) {
-        const { signed, loading } = useContext(AuthContext);
-        return signed ? children : <Navigate to="/" />;
-    }
+    const { signed, loading } = useContext(AuthContext);
 
     return (
         <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/register" element={<SignUp />} />
-            <Route path="/home" element={<PrivateRoute />}>
-                <Route element={<Dashboard />} />
-            </Route>
+            {!signed && (
+                <>
+                    <Route path="/auth" element={<SignIn />} />
+                    <Route path="/register" element={<SignUp />} />
+                </>
+
+            )}
+            {signed && (
+                <Route path="/dashboard" element={<Dashboard />} />
+            )}
+            <Route path="*" element={signed ? <h1>Not found</h1> : <Navigate to="/auth" />} />
         </Routes>
     )
 }
