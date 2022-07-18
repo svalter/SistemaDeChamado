08e27a98dca14c7a259bfc64ae815c045e9a9ecf
Valter Silva <svalter222@gmail.com>
2022-07-18 13:48:05 -0300

    dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,38 @@
-import React from "react";
-import  styled from './Dashboard.module.css';
+import React, { useState } from "react";
+import styled from './Dashboard.module.css';
 import Sidebar from '../../components/Sidebar';
 import Title from '../../components/Title';
-import { FiHome } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
+
+    const [called, setCalled] = useState([]);
+
     return (
-      <div>
+        <div>
             <Sidebar />
             <div className={styled.content}>
-                <Title name="Dashboard">
-                    <FiHome size={25}/>
+                <Title name="Atendimentos">
+                    <FiMessageSquare size={25} />
                 </Title>
+
+                {called.length === 0 ? (
+                    <div className={styled.container}>
+                        <span>Nenhum chamado registrado...</span>
+
+                        <Link to="/new" className={styled.buttonNewCalled}>
+                            <FiPlus color="#FFFFFF" size={25}></FiPlus>
+                            Novo chamado
+                        </Link>
+                    </div>
+                ):
+                <div className={styled.container}>
+                    <span>chamado 1</span>
+                </div>
+                }
             </div>
         </div>
     );
-  }
+}
 
-  
