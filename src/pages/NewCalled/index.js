b1aea712185a043d18f3710af4283ebddf69f43f
Valter Silva <svalter222@gmail.com>
2022-07-20 13:03:13 -0300

    modal chamados

--- src/pages/NewCalled/index.js
+++ src/pages/NewCalled/index.js
@@ -3,13 +3,14 @@ import styled from './NewCalled.module.css';
 import { toast } from 'react-toastify';
 import Sidebar from '../../components/Sidebar';
 import Title from '../../components/Title';
+import { Link } from "react-router-dom";
 import { FiPlus } from 'react-icons/fi';
 import firebase from '../../services/firebaseConnection';
 import { AuthContext } from '../../contexts/auth';
 
 export default function NewCalled() {
 
-    const [loadCustumers, setLoadCustumers] = useState(true);   
+    const [loadCustumers, setLoadCustumers] = useState(true);
     const [custumers, setCustumers] = useState([]);
     const [custumerSelected, setCustumerSelected] = useState(0);
     const [description, setDescription] = useState('Suporte');
@@ -21,26 +22,26 @@ export default function NewCalled() {
     useEffect(() => {
         async function loadCustumers() {
             let lista = [];
-            await  firebase.firestore().collection('custumers')
-            .get()
-            .then((snapshot)=> {
-                
-                snapshot.forEach((doc) => {
-                    lista.push({
-                        id: doc.id,
-                        nameCompany: doc.data().nameCompany
+            await firebase.firestore().collection('custumers')
+                .get()
+                .then((snapshot) => {
+
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id: doc.id,
+                            nameCompany: doc.data().nameCompany
+                        })
                     })
+                    if (lista.length === 0) {
+                        setCustumers([{ id: '1', nameCompany: 'FREELA' }]);
+                        setLoadCustumers(false);
+                        return;
+                    }
                 })
-                if(lista.length === 0) {
-                    setCustumers([ { id: '1', nameCompany: 'FREELA'}]);
+                .catch((error) => {
                     setLoadCustumers(false);
-                    return;
-                }
-            })
-            .catch((error) =>{
-                setLoadCustumers(false);
-                setCustumers([ { id:'1', nameCompany: ''}]);
-            })
+                    setCustumers([{ id: '1', nameCompany: '' }]);
+                })
 
             setCustumers(lista);
             setLoadCustumers(false);
@@ -53,24 +54,24 @@ export default function NewCalled() {
         e.preventDefault();
 
         await firebase.firestore().collection('called')
-        .add({
-           created: new Date(),
-           custumer: custumers[custumerSelected].nameCompany,
-           custumerId:  custumers[custumerSelected].id,
-           description: description,
-           status: status,
-           complement: complement,
-           userId: user.uid
-        })
-        .then(() => {
-            toast.success('Chamado cadastrado com sucesso!');
-            setComplement('');
-            setCustumerSelected(0);
-        })
-        .catch((error) => {
-            toast.error('Ops, erro ao registrar!')
-            console.log(error)
-        })
+            .add({
+                created: new Date(),
+                custumer: custumers[custumerSelected].nameCompany,
+                custumerId: custumers[custumerSelected].id,
+                description: description,
+                status: status,
+                complement: complement,
+                userId: user.uid
+            })
+            .then(() => {
+                toast.success('Chamado cadastrado com sucesso!');
+                setComplement('');
+                setCustumerSelected(0);
+            })
+            .catch((error) => {
+                toast.error('Ops, erro ao registrar!')
+                console.log(error)
+            })
     }
 
     function handleChangeSelect(e) {
@@ -86,6 +87,8 @@ export default function NewCalled() {
 
     }
 
+    console.log("caralho", custumers.length === 0);
+
     return (
         <div>
             <Sidebar />
@@ -94,23 +97,32 @@ export default function NewCalled() {
                     <FiPlus size={25} />
                 </Title>
                 <div className={styled.container}>
+                    {custumers.length === 0 ? (
+                        <div className={styled.contentCustumer}>
+                        <span>Nenhum cliente registrado...</span>
+
+                        <Link to="/custumers" className={styled.buttonNewCustumer}>
+                            <FiPlus color="#FFFFFF" size={25}></FiPlus>
+                            Novo cliente
+                        </Link>
+                    </div>
+                    ) :
+                    (
+
                     <div>
                         <form className={styled.containerForm} onSubmit={handleRegister}>
-                     
+
                             <label>Cliente</label>
                             <select className={styled.select} value={custumerSelected} onChange={handleChangeCustumers}>
-                                <option key={1} value={1}>
-                                    Sujeito Programador
-                                </option>
                                 {custumers.map((item, index) => {
-                                    return(
+                                    return (
                                         <option key={item.id} value={index}>
                                             {item.nameCompany}
                                         </option>
                                     )
                                 })}
                             </select>
-                            
+
                             <label>Assunto</label>
                             <select className={styled.select} value={description} onChange={handleChangeSelect}>
                                 <option value="Suporte">Suporte</option>
@@ -118,7 +130,7 @@ export default function NewCalled() {
                                 <option value="Financeiro">Financeiro</option>
                             </select>
 
-                                         
+
                             <label>Status</label>
                             <div className={styled.status}>
                                 <input
@@ -154,16 +166,18 @@ export default function NewCalled() {
                                 type="text"
                                 placeholder="Descreva seu problema (opcional)"
                                 value={complement}
-                                onChange={ (e) => setComplement(e.target.value)}>
+                                onChange={(e) => setComplement(e.target.value)}>
                             </textarea>
 
-                            <div className={styled.actions}> 
+                            <div className={styled.actions}>
                                 <button type="submit" className={styled.buttonToSave}>
                                     Cadastrar
                                 </button>
                             </div>
                         </form>
                     </div>
+                    )
+                    }
                 </div>
             </div>
         </div>
--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -7,13 +7,13 @@ import 'firebase/compat/storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWA-mxD3zAa0ddyV3th8Mgni2_6YGdUQc",
-  authDomain: "sistema-chamado-b1cac.firebaseapp.com",
-  projectId: "sistema-chamado-b1cac",
-  storageBucket: "sistema-chamado-b1cac.appspot.com",
-  messagingSenderId: "1053619923651",
-  appId: "1:1053619923651:web:09e7a4dfb96b53996d1076",
-  measurementId: "G-R00PDZVYFZ"
+  apiKey: "AIzaSyAeZ0C5ZDR5kbMpseqiBHt7GWT4jnCHMPY",
+  authDomain: "ticket-system-cf5a8.firebaseapp.com",
+  projectId: "ticket-system-cf5a8",
+  storageBucket: "ticket-system-cf5a8.appspot.com",
+  messagingSenderId: "955642063006",
+  appId: "1:955642063006:web:4c0e9aebd91933cff8338f",
+  measurementId: "G-6GLLN14EM5"
 };
 
 // Initialize Firebase
